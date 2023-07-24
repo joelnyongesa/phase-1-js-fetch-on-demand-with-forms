c4afa7598306a3d057406553df407beb545fcd50
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-24 22:44:09 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+// Task is to get the form working.
+
 const init = () => {
-  
+    // What function should do: Add event listener
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID')
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
